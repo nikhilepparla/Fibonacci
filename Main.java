1d084412540334ad4e55d8287ea5db6aca992285
nikhilepparla <e.nikhil1999@gmail.com>
2023-10-16 21:51:10 +0530

    Without recurssion

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n1 = 0, n2 = 1, n3;
 
-    public static void fibo(int number) {
-        if (number > 0) {
-            fibo(number - 1);
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-            System.out.println("Fibonacci values are " + n3);
-        }
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        fibo(10);
+        int number = sc.nextInt();
+        int n1 = 0, n2 = 1, n3 = 0;
+        for (int i = 0; i < number; i++) {
+            n3 = n1 + n2;
+            System.out.println("Value is " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
     }
 }
